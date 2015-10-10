1f0ddcbb28ba5fde097c3d955264bc835a59a46b
Idan <idan.hovav16@gmail.com>
2015-10-09 22:45:16 -0700

    Updated server. Moved index to static

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
-from flask import Flask
+"""Idan Hovav
+Hacking shit and shit.
+"""
+
+import sqlite3
+import time
+from flask import Flask, request, g, redirect, render_template
+
+app = Flask(__name__)
+DATABASE = 'bt.db'
+
+@app.route("/")
+def hello():
+	return "Swando"
+	#return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run()
