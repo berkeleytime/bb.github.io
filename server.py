5cfa01f1b4ef93a1154d5a1206d82f96a92dadc9
Idan <idan.hovav16@gmail.com>
2015-10-09 22:54:01 -0700

    1 site plz

--- server.py
+++ server.py
@@ -11,8 +11,7 @@ DATABASE = 'bt.db'
 
 @app.route("/")
 def hello():
-	return "Swando"
-	#return render_template("index.html")
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	app.run()
